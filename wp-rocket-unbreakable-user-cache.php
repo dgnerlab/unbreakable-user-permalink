c7e842963243bacfc4eef141ad4115d42f0947d8
Jaemin S <81064063+dgnerlab@users.noreply.github.com>
2022-08-27 05:09:12 +0900

    Add files via upload

---
+++ wp-rocket-unbreakable-user-cache.php
@@ -0,0 +1,40 @@
+<?php
+
+/* * * * *
+
+
+
+* * * * */
+
+/* WP Rocket - init */
+add_action( 'init', 'dgner_online' );
+add_filter( 'home_url', 'dgner_online' );
+add_filter( 'page_link', 'dgner_online' );
+add_filter( 'post_link', 'dgner_online' );
+add_filter( 'post_type_link', 'dgner_online' );
+// add_filter( 'category_link', 'dgner_online' ); // ┓ = Add if necessary.
+// add_filter( 'tag_link', 'dgner_online' );      // ┃
+// add_filter( 'author_link', 'dgner_online' );   // ┃
+// add_filter( 'day_link', 'dgner_online' );      // ┃
+// add_filter( 'month_link', 'dgner_online' );    // ┃
+// add_filter( 'year_link', 'dgner_online' );     // ┛
+add_action( 'template_redirect', 'dgner_offline' );
+
+function dgner_online( $permalink ) {
+    if( is_user_logged_in() && is_page() ) {
+        return add_query_arg( 'login', 'yes', trailingslashit( $permalink ) ); // trailingslashit() = Appends a trailing slash. Will remove trailing forward and backslashes if it exists already before adding a trailing forward slash.
+    }
+    else {
+        return $permalink;
+    }
+}
+
+function dgner_offline() {
+    $entities = htmlentities( $_SERVER['REQUEST_URI'] );
+    $loginyes = '/?login=yes';
+    $online = strpos( $entities, $loginyes );
+    if( ! is_user_logged_in() && is_page() && $online !== false ) {
+        wp_safe_redirect( home_url(), 301 );
+        exit;
+    }
+}
